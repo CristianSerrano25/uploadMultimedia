7477604915acfc229356173d25f7a8b84d072e37
CristianSerrano25 <cristianserrano79@gmail.com>
2024-10-16 16:20:40 -0300

    validations added

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { uploadImage } from "../middlewares/upload.middleware.js";  
 import { createProduct } from "../controllers/pruducts.controllers.js";
+import { applyValidations } from "../middlewares/applyValidations.middleware.js";
+import { createProductSchema } from "../validations/product.valitatios.js";
 
 const productsRouter = Router();
 
-productsRouter.post('/', uploadImage("product"), createProduct);
+productsRouter.post('/', uploadImage("product"),applyValidations(createProductSchema), createProduct);
 
 export { productsRouter };
